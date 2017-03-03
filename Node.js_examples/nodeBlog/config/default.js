719589ac19aae746f8a64bf3778ad83bb72a54f7
lifeng1893 <lifeng.isme@foxmail.com>
2017-03-03 18:13:04 +0800

    This is only for canvas and css3

--- Node.js_examples/nodeBlog/config/default.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  port: 8888,
-  session:{
-    secret: 'myblog',
-    key: 'myblog',
-    maxAge: 2592000000
-  },
-  mongodb: 'mongodb://localhost:27017/myblog'
-};
--- Node.js_examples/nodeBlog/routes/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(app) {
-  app.get('/', function(req, res) {
-    res.render('posts');
-  });
-  app.use('/signup', require('./signup'));
-  app.use('/login', require('./login'));
-  app.use('/logout', require('./logout'));
-  app.use('/posts', require('./posts'));
-};
